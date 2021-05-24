b1bd405509a66ecb2f2d68ad6357c15ed577a89a
Samuelisch <samuelchanwk@hotmail.com>
2021-05-24 20:04:59 +0800

    completed form - scripts support next

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,28 @@
 /* dropdown menu function */
-const menuBtn = document.querySelector('.dropdown-btn');
+const menuBtn = document.querySelector('.dropdown.btn');
 const dropMenu = document.querySelector('.drop-content');
 const plusBtn = document.querySelector('.plus-minus-btn');
+const transactForm = document.querySelector('.dropdown-transact');
 const plusBtnContainer = document.querySelector('.plus-minus');
 const amount = document.querySelector('.transact-amount');
+const date = document.querySelector('.date');
 
 function outside(e) {
-  if (!e.target.matches('.dropdown-btn') && (!e.target.matches('.dropdown-icon'))) {
+  if (!e.target.matches('.dropdown')) {
     if (dropMenu.classList.contains('show-dropdown')) {
       dropMenu.classList.remove('show-dropdown');
     }
   } else {
     dropMenu.classList.toggle('show-dropdown');
   }
-  if (e.target.matches('.plus-minus-btn') || (e.target.matches('.plus-minus'))) {
+  /* slider */
+  if (e.target.matches('.slider')) {
     plusMinus();
+  }
+  /* transact button */
+  if (e.target.matches('.transact')) {
+    document.querySelector('.transact').disabled = true;
+    transactForm.classList.toggle('show-dropdown');
   }
 }
 
@@ -22,14 +30,23 @@ function plusMinus() {
   if (plusBtn.style.transform) {
     plusBtn.style.transform = null;
     plusBtn.textContent = "-";
-    plusBtnContainer.style.backgroundColor = "rgb(156, 30, 30)";
-    amount.style.color= "rgb(156, 30, 30)"
+    plusBtnContainer.style.backgroundColor = "var(--red)";
+    amount.style.color= "var(--red)"
   } else {
     plusBtn.style.transform = "translateX(100%)"
     plusBtn.textContent = "+";
-    plusBtnContainer.style.backgroundColor = "rgb(32, 146, 32)";
-    amount.style.color= "rgb(32, 146, 32)";
+    plusBtnContainer.style.backgroundColor = "var(--green)";
+    amount.style.color= "var(--green)";
   }
 }
 
+function getDateToday() {
+  function zerofill(i) {
+    return (i < 10 ? '0' : '') + i;
+  }
+  let today = new Date;
+  return `${today.getFullYear()}-${zerofill(today.getMonth() + 1)}-${zerofill(today.getDate())}`;
+}
+
 window.addEventListener('click', outside);
+date.value = getDateToday();
